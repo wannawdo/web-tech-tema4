3c7069fdafd624f2ed5474409be808e47ca2d105
Oana Dragomirescu <wannawdo@gmail.com>
2021-01-20 08:39:17 +0200

    TEMA 4

--- main/src/components/RobotForm.js
+++ main/src/components/RobotForm.js
@@ -1,73 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class RobotForm extends Component {
-    constructor(props){
-        super();
-        this.state={
-        name:"",
-        type:"",
-        mass:0,
-		
-    }; 
-    this.handleChangeName=this.handleChangeName.bind(this);
-    this.handleChangeType=this.handleChangeType.bind(this);
-    this.handleChangeMass=this.handleChangeMass.bind(this);
-	}
-    
-    handleChangeName(event) {
-        this.setState({ name: event.target.value });
-      }
-      handleChangeType(event) {
-        this.setState({ type: event.target.value });
-      }
-      handleChangeMass(event) {
-        this.setState({ mass: event.target.value });
-      }
+  constructor() {
+    super();
+    this.state = {
+      type: "",
+      name: "",
+      mass: 0
+    };
 
-      onAdd = () => {
-        this.props.onAdd(this.state.name,this.state.type,this.state.mass)
-    }
-  //  onAdd(){
-    //     if (!this.state.name || !this.state.type || !this.state.mass)
-    //     console.log("Fields cannot be empty!");
-    //   else {
-    //     let newRobot = {
-    //       name: this.state.name, 
-    //       type:this.state.type,
-    //       mass: this.state.mass,
-    //     };
- //   }
+    this.handleChange = evt => {
+      this.setState({
+        [evt.target.name]: evt.target.value
+      });
+    };
+  }
 
   render() {
-  	
     return (
       <div>
-          <input id="name"
-              onChange={this.handleChangeName}
-              type="text"
-              name="name"
-              
-              value={this.state.name}
-            />
-            <input id="type"
-              onChange={this.handleChangeType}
-              type="text"
-              name="type"
-         
-              value={this.state.type}
-            />
-            <input id="mass"
-              onChange={this.handleChangeMass}
-              type="text"
-              name="mass"
-            
-              value={this.state.mass}
-            />
-  		<button onClick={()=>this.props.onAdd()}>Add Robot</button>
-          
+        <input type="text" name="type" id="type" onChange={this.handleChange} />
+        <input type="text" name="name" id="name" onChange={this.handleChange} />
+        <input
+          type="number"
+          name="mass"
+          id="mass"
+          onChange={this.handleChange}
+        />
+        <input
+          type="button"
+          value="add"
+          onClick={() =>
+            this.props.onAdd({
+              name: this.state.name,
+              type: this.state.type,
+              mass: this.state.mass
+            })
+          }
+        />
       </div>
-    )
+    );
   }
 }
 
-export default RobotForm
+export default RobotForm;
--- main/src/components/RobotList.js
+++ main/src/components/RobotList.js
@@ -4,52 +4,40 @@ import Robot from './Robot'
 import RobotForm from './RobotForm'
 
 
-
 class RobotList extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots : []
-		}
-		this.onAdd=this.onAdd.bind(this);
+	constructor() {
+	  super();
+	  this.state = {
+		robots: []
+	  };
+  
+	  this.add = robot => {
+		this.store.addRobot(robot);
+	  };
 	}
-	componentDidMount(){
-		this.store = new RobotStore()
+  
+	componentDidMount() {
+	  this.store = new RobotStore();
+	  this.setState({
+		robots: this.store.getRobots()
+	  });
+	  this.store.emitter.addListener("UPDATE", () => {
 		this.setState({
-			robots : this.store.getRobots()
-		})
-		this.store.emitter.addListener('UPDATE', () => {
-			this.setState({
-				robots : this.store.getRobots()
-			})			
-		})
+		  robots: this.store.getRobots()
+		});
+	  });
 	}
-
-	onAdd(name, type, mass){
-		if (!name || !type || !mass)
-		    console.log("Fields cannot be empty!");
-		  else {
-		    let newRobot = {
-		      name: name, 
-		      type:type,
-		      mass:mass,
-			};
-		}
-    }
-
+  
 	render() {
-		return (
-			<div>
-				 <RobotForm onAdd={this.onAdd}/>
-				{
-					this.state.robots.map((e, i) => 
-						<Robot item={e} key={i} />
-					)
-				}
-			</div>
-		)
+	  return (
+		<div>
+		  {this.state.robots.map((e, i) => (
+			<Robot item={e} key={i} />
+		  ))}
+		  <RobotForm onAdd={this.add} />
+		</div>
+	  );
 	}
-}
-
-
-export default RobotList
+  }
+  
+  export default RobotList;
